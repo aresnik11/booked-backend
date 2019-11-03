9444508bf79d63305596fafbf2ca7ae8c72f5b2b
aresnik11 <aresnik11@gmail.com>
2019-11-03 18:24:09 -0500

    add default book lists on user creation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,6 +13,12 @@ class Api::V1::UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
+            #automatically create default booklists on creation of a new user
+            BookList.create({name: "Favorites", user: user})
+            BookList.create({name: "Read", user: user})
+            BookList.create({name: "Want to read", user: user})
+            # BookList.create({name: "Shared with me", user: user})
+            
             token = encode_token(user.id)
             render json: { user: UserSerializer.new(user), token: token }, status: :created
         else
