fb419431889eaf914d1f04e1af794c8cbb53c2d0
aresnik11 <aresnik11@gmail.com>
2019-11-05 23:02:22 -0500

    updated to exclude free preview books from api response

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -37,11 +37,12 @@ class Api::V1::BooksController < ApplicationController
         searchable_term = search_term.gsub(/\s/,'+')
         #get books with that search term
         #format is q="search+term" for a search with that exact string
+        #-"free+preview" is to exclude free preview books
         #author/genre/title search format is q=intitle:"search+term" q=inauthor:"search+term" q=subject:"search+term"
         #search type is empty string if all, or the specific search format (i.e. intitle:)
         #langRestrict=en is restricting to only english books
         #&filter=partial to not get dupes in All search - otherwise get results for viewability:"partial" and viewability:"no_pages"
-        response_string = RestClient.get("https://www.googleapis.com/books/v1/volumes?q=#{search_type}\"#{searchable_term}\"&langRestrict=en&filter=partial&maxResults=40&startIndex=#{start_index}&key=#{API_KEY}")
+        response_string = RestClient.get("https://www.googleapis.com/books/v1/volumes?q=-\"free+preview\"#{search_type}\"#{searchable_term}\"&langRestrict=en&filter=partial&maxResults=40&startIndex=#{start_index}&key=#{API_KEY}")
         response_hash = JSON.parse(response_string)
         #send the books response back to the frontend in json
         #small thumbnail zoom=5
