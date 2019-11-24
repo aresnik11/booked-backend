9289fb8f1cd3a158f9162b2f12a1ed6e724505cb
aresnik11 <aresnik11@gmail.com>
2019-11-24 14:35:12 -0500

    updated from front end urlS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://booked.netlify.com', 'localhost:3000'
 
     resource '*',
       headers: :any,
