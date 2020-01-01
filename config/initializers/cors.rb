da1029840df96a4ca321eb02a3f550a17dc5e87e
aresnik11 <aresnik11@gmail.com>
2020-01-01 15:46:36 -0500

    removing localhost from cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://booked.netlify.com', 'localhost:3000'
+    origins 'https://booked.netlify.com'
 
     resource '*',
       headers: :any,
